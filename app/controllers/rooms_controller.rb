8342a401bc0213999b174d155c5eab1d91b8c6a5
Eduardo Moreno <ejmm320@gmail.com>
2017-04-21 21:09:19 -0400

    Bootstrap 4 support added

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,7 +1,6 @@
 class RoomsController < ApplicationController
   def show
-    user = User.find(session[:current_user])
-    dialect = Dialect.find(session[:dialect])
-    render text: "User: #{user.username} who wants to speak in: #{dialect.name}"
+    @user = User.find(session[:current_user])
+    @dialect = Dialect.find(session[:dialect])
   end
 end
