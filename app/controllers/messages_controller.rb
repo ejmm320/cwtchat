534d76a01ef4f5838d266396086c7030d8def97d
Eduardo Moreno <ejmm320@gmail.com>
2017-04-21 23:46:22 -0400

    Added view for partial messages rendering without real-time behaviour and some styles

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,9 +7,14 @@ class MessagesController < ApplicationController
     @message = Message.new(message_params)
     @message.user_id = session[:current_user]
     @message.dialect = @dialect
-    uri = URI("#{@dialect.apiurl}?text=#{URI.encode(@message.content)}")
-    response = JSON.parse(Net::HTTP.get(uri))
-    render text: "Usuario: #{@message.user_id}, Mensaje: #{response["contents"]["translated"]}, Dialecto: #{@dialect.apiurl}"
+    #uri = URI("#{@dialect.apiurl}?text=#{URI.encode(@message.content)}")
+    #response = JSON.parse(Net::HTTP.get(uri))
+    #@message.content_translated = response["contents"]["translated"]
+    @message.content_translated = @message.content
+    if @message.save
+      ActionCable.server.broadcast "room_channel", message: @message.content_translated
+    end
+    #render text: "Usuario: #{@message.user_id}, Mensaje: #{response["contents"]["translated"]}, Dialecto: #{@dialect.apiurl}"
     #@message.save
   end
 
